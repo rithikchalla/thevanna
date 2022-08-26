d02c8aa4b700487807e4d0d954f14ed5ebb65596
rithik <19rithik@gmail.com>
2022-08-25 20:02:01 -0400

    Modified header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thevanna';
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home'
+      },
+      {
+        label: 'About Us'
+      },
+      {
+        label: 'Contact'
+      }
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InteriorComponent } from './interior/interior.component';
 import { HomeComponent } from './home/home.component';
+import { DividerModule } from 'primeng/divider';
+import { MenubarModule } from 'primeng/menubar'
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DividerModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
